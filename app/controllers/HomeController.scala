0df85403deaedae6a0fc69459d354831fe74d7da
Kaj Ström <kaj.strom@kapsi.fi>
2017-05-25 18:30:25 +0300

    Form for adding a Todo task. No handling yet

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,24 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
+import play.api.i18n._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  val todoForm = Form(
+    mapping(
+      "todo" -> text
+    )(TodoData.apply)(TodoData.unapply)
+  )
 
   /**
    * Create an Action to render an HTML page.
@@ -19,6 +28,26 @@ class HomeController @Inject() extends Controller {
    * a path of `/`.
    */
   def index = Action { implicit request =>
-    Ok(views.html.index())
+    Ok(views.html.index(todoForm))
+  }
+
+  def add = Action { implicit request =>
+    val errorFunction = { formWithErrors: Form[TodoData] =>
+      // This is the bad case, where the form had validation errors.
+      // Let's show the user the form again, with the errors highlighted.
+      // Note how we pass the form with errors to the template.
+      BadRequest(views.html.index(formWithErrors))
+    }
+
+    val successFunction = { data: TodoData =>
+      // This is the good case, where the form was successfully parsed as a Data.
+
+      Redirect(routes.HomeController.index()).flashing("info" -> "Todo task added!")
+    }
+
+    val formValidationResult = todoForm.bindFromRequest
+    formValidationResult.fold(errorFunction, successFunction)
   }
 }
+
+case class TodoData(todo: String)
